84c7bdb5c2a51d735469c5452d3fe6cbab60eff1
Alexandre Bulcao <ambulcao@gmail.com>
2021-01-29 20:13:40 +0000

    Criacao do banner e buttons

--- src/Banner.js
+++ src/Banner.js
@@ -21,12 +21,31 @@ function Banner() {
     console.table(movie);
 
     return (
-        <header> {/** Background image */}
-            {/** title */}
-            {/** div > 2 buttons */}
-            {/** description */}            
+        <header 
+            className="banner"
+            style={{ 
+                backgroundSize: "cover",
+                backgroundImage: `url(
+                    "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+                )`,
+                backgroundPosition: "center center",
+            }}
+        > {/** Background image */}
+            <div className="banner_contents">
+                <h1>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                {/** div > 2 buttons */}
+                {/** description */}            
+                
+            </div>
         </header>
-    )
+    );
 }
 
 export default Banner;
