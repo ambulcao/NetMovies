b6b10dfc8f21b7746ca0133219498f5f91a06c46
Alexandre Bulcao <ambulcao@gmail.com>
2021-01-29 21:06:28 +0000

    Ajustando banner principal e botoes

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -20,6 +21,10 @@ function Banner() {
 
     console.table(movie);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n -1) + "..." : str;
+    }
+
     return (
         <header 
             className="banner"
@@ -32,7 +37,7 @@ function Banner() {
             }}
         > {/** Background image */}
             <div className="banner_contents">
-                <h1>
+                <h1 className="banner_title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
 
@@ -40,10 +45,13 @@ function Banner() {
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
-                {/** div > 2 buttons */}
-                {/** description */}            
                 
+                <h1 className="banner_description">
+                    {truncate(movie?.overview, 150)}
+                </h1>            
             </div>
+
+            <div className="banner_fadeBottom"/>
         </header>
     );
 }
